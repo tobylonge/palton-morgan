f8864dacdd863a8c5c1cdbfb942cc07f05f277ef
Oluwatobi <tobylonge@gmail.com>
2021-07-06 18:20:03 +0100

    addded service worker offline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,3 +9,13 @@ import "./src/styles/tailwind.css"
 import "./src/styles/slick.scss"
 import "./src/styles/slick-theme.scss"
 import "./src/styles/custom.scss"
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
